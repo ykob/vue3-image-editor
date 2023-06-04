3973d980d1727cc668b9bb28dd860decc2534445
ykob <info@tplh.net>
2023-06-04 22:23:54 +0900

    added canvas element on App.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
-import { SelectFileButton } from "./components/functional/";
+import { onMounted, ref } from "vue";
+import {
+  DownloadImageButton,
+  SelectFileButton,
+} from "./components/functional/";
+
+const canvas = ref<HTMLCanvasElement | null>(null);
+let ctx: CanvasRenderingContext2D | null = null;
+
+onMounted(async () => {
+  ctx = canvas.value?.getContext("2d") ?? null;
+});
 </script>
 
 <template>
-  <SelectFileButton />
+  <div>
+    <SelectFileButton />
+    <DownloadImageButton :canvas="canvas" />
+    <canvas width="640" height="640" ref="canvas"></canvas>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
