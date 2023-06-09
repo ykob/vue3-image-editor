c9e5d4131de8af2d09cb462055bbfec843c8e064
ykob <info@tplh.net>
2023-06-09 19:06:22 +0900

    put the icons for Control Buttons.

--- src/components/functional/ControlButtons.vue
+++ src/components/functional/ControlButtons.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
-import { ButtonFilled } from "../ui/";
+import {
+  mdiArrowCollapse,
+  mdiArrowExpand,
+  mdiRotateLeft,
+  mdiRotateRight,
+} from "@mdi/js";
+import { ButtonFilled, IconBase } from "../ui/";
 
 defineEmits(["rotateLeft", "rotateRight", "scaleDown", "scaleUp"]);
 </script>
 
 <template>
   <div class="control-buttons">
-    <ButtonFilled circle @click="$emit('scaleDown')"> 縮小 </ButtonFilled>
-    <ButtonFilled circle @click="$emit('scaleUp')"> 拡大 </ButtonFilled>
-    <ButtonFilled circle @click="$emit('rotateLeft')"> 左回転 </ButtonFilled>
-    <ButtonFilled circle @click="$emit('rotateRight')"> 右回転 </ButtonFilled>
+    <ButtonFilled circle @click="$emit('scaleDown')">
+      <IconBase :path="mdiArrowCollapse" name="縮小" />
+    </ButtonFilled>
+    <ButtonFilled circle @click="$emit('scaleUp')">
+      <IconBase :path="mdiArrowExpand" name="拡大" />
+    </ButtonFilled>
+    <ButtonFilled circle @click="$emit('rotateLeft')">
+      <IconBase :path="mdiRotateLeft" name="左回転" />
+    </ButtonFilled>
+    <ButtonFilled circle @click="$emit('rotateRight')">
+      <IconBase :path="mdiRotateRight" name="右回転" />
+    </ButtonFilled>
   </div>
 </template>
 
