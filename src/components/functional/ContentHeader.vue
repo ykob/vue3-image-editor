9c10dfa502d69a2636b097576cea4d77256e28f1
ykob <info@tplh.net>
2023-06-18 21:44:32 +0900

    added github and twitter button.

--- src/components/functional/ContentHeader.vue
+++ src/components/functional/ContentHeader.vue
@@ -1,14 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { mdiGithub, mdiTwitter } from "@mdi/js";
+import { ButtonFilled, IconBase } from "../ui/";
+</script>
 
 <template>
   <div class="content-header">
     <h1>Vue3 Image Editor</h1>
+    <div class="buttons">
+      <ButtonFilled circle href="" target="_blank" size="sm">
+        <IconBase :path="mdiGithub" name="GitHub" size="sm" />
+      </ButtonFilled>
+      <ButtonFilled circle href="" target="_blank" size="sm">
+        <IconBase :path="mdiTwitter" name="Twitter" size="sm" />
+      </ButtonFilled>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.content-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 24px;
+}
 h1 {
   margin-block: 0;
   letter-spacing: 0.06em;
+}
+.buttons {
+  display: flex;
+  gap: 8px;
 }
 </style>
